2c6ac031dfc20314e3951b1ebe62a13e28012d7d
tom lee <tdroid99@gmail.com>
2018-08-01 11:19:06 -0600

    made migrations and knexfile.js

---
+++ migrations/20180801111212_cats_breeds.js
@@ -0,0 +1,8 @@
+//run this (export.up) when you migrate:latest
+exports.up = function(knex, Promise) {
+  
+};
+// run this (export.down) when you migrate:rollback
+exports.down = function(knex, Promise) {
+  
+};
