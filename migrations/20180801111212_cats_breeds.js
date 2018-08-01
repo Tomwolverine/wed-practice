d39466a83f251edc474e0bb2e21ece75ce267543
tom lee <tdroid99@gmail.com>
2018-08-01 11:46:15 -0600

    seeded table

--- migrations/20180801111212_cats_breeds.js
+++ migrations/20180801111212_cats_breeds.js
@@ -1,8 +1,12 @@
 //run this (export.up) when you migrate:latest
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('cat_breeds', (table) => {
+        table.increments()
+        table.text('breed')
+        table.integer('popularity')
+    })
 };
 // run this (export.down) when you migrate:rollback
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('cat_breeds')
 };
