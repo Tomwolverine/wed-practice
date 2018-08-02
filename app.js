ad6a8ba81f6420821a9b372b72eed26208d6d1eb
tom lee <tdroid99@gmail.com>
2018-08-02 10:55:36 -0600

    added database-connection.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const port = 7000
 const app = express()
+const port = 7000
+const queries = require('./queries')
 
 app.get('/',(request,response,next) => {
-    response.send('scary cat')
+    queries.getAll().then(cats => response.json({cats}))
 })
-
 
 app.listen(port, () => {
     console.log(`running on port ${port}`)
